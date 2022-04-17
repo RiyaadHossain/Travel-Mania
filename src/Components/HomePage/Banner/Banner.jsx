68320ed31769f20d640dc1828c98bdae0b2d4790
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-04-17 15:36:33 +0600

    Banner Section added

--- src/Components/HomePage/Banner/Banner.jsx
+++ src/Components/HomePage/Banner/Banner.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <div>
-            <h1>Banner is here</h1>
+        <div className='banner-container text-center h-[93vh] flex items-center text-white'>
+            <div>
+                <h1 className='text-6xl font-bold'>Life is short and the world is wide!</h1>
+                <p className='text-xl my-7 font-thin text-slate-300'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate eligendi accusantium ipsa totam quis explicabo cum doloremque possimus cumque omnis.</p>
+                <button className='bg-red-700 py-2 px-5 rounded text-lg text-white'>Explore Now</button>
+            </div>
         </div>
     );
 };
