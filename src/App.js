2d41923e6f44d002987f56196c9f4304ee631008
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-04-17 14:42:49 +0600

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Components/About/About';
+import LogIn from './Components/Auth/LogIn/LogIn';
+import SignUp from './Components/Auth/SignUp/SignUp';
+import Blogs from './Components/Blogs/Blogs';
+import CheckOut from './Components/CheckOut/CheckOut';
+import Home from './Components/HomePage/Home/Home';
+import NotFound from './Components/NotFound/NotFound';
+import RequireAuth from './Components/RequireAuth/RequireAuth';
+import Footer from './Components/Shared/Footer/Footer';
+import Navbar from './Components/Shared/Navbar/Navbar';
+
 
 function App() {
   return (
     <div>
+      <Navbar/>
       <Routes>
-        <Route/>
+        <Route path='/' element={<Home />}/>
+        <Route path='/blogs' element={<Blogs />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/login' element={<LogIn />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='checkout' element={<RequireAuth>
+          <CheckOut/>
+        </RequireAuth> }/>
+        <Route path='*' element={<NotFound />}/>
       </Routes>
+      <Footer/>
     </div>
   );
 }
--- src/Components/Shared/Navbar/Navbar.jsx
+++ src/Components/Shared/Navbar/Navbar.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Nabvar.css'
 
 const Navbar = () => {
     return (
-        <div>
-            
+        <div className='container mx-auto flex justify-between items-center h-16'>
+            <div>
+                <h1 className='text-2xl font-bold'>Logo</h1>
+            </div>
+            <nav>
+                <NavLink className={({isActive}) => isActive ? 'active-link' : 'link'} to='/'>Home</NavLink>
+                <NavLink className={({isActive}) => isActive ? 'active-link' : 'link'} to='/blog'>Blog</NavLink>
+                <NavLink className={({isActive}) => isActive ? 'active-link' : 'link'} to='/about'>About</NavLink>
+                <NavLink className='button' to='/login'>Log In</NavLink>
+            </nav>
         </div>
     );
 };
