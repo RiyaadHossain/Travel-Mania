ae7b59f4adcdf0badbbe1169dca3391f6e830718
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-04-18 23:43:36 +0600

    Success page styled

--- src/Components/SuccessMsg/SuccessMsg.jsx
+++ src/Components/SuccessMsg/SuccessMsg.jsx
@@ -3,9 +3,9 @@ import Success from '../../Assets/Image/2dce85df72ed99399fc68d9bec519ce0.gif'
 
 const SuccessMsg = () => {
     return (
-        <div className='h-[69.2vh] flex justify-center items-center'>
+        <div className='h-[69.3vh]  flex justify-center items-center'>
             <div>
-                <img src={Success} alt="" />
+                <img className='max-h-[500px]'  src={Success} alt="" />
             </div>
         </div>
     );
